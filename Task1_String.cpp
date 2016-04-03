c718f6819e5a6ec1d65cc3b9c8207f7c023d1939
codevzasade <codevzasade@gmail.com>
2016-04-04 01:41:10 +0400

    Part2
    Найти слова, первый и последний символы которых совпадают, и
    вывести эти слова и их количество.

---
+++ Task1_String.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+#define NUM 1000
+
+int main()
+{
+	int i, g, item = 0, j = 0;
+	char str[NUM];
+	srand(time(NULL));
+	for (i = 0; i < NUM; i++)
+	{
+		if (i == NUM - 1)
+			str[i] = '\0';
+		else if (i % 9 == 0 && i > 0)
+			str[i] = ' ';
+		else
+			str[i] = rand() % 26 + 97;
+	}
+	printf("Source string:\n%s\n", str);
+	printf("\nResult string:\n");
+	i = 0;
+	for (i; i < NUM; i++)
+	{
+		j += 1;
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			g = i - (j - 1); // определяем первый символ слова
+			j = 0; // обнуляем для подсчета новых элементов
+			if (str[g] == str[i - 1])
+			{
+				// printf("g: %d , i: %d\n", g, i);
+				item += 1;
+				for (g; g < i; g++)
+				{
+					printf("%c", str[g]);
+				}
+				printf(" ");
+			}
+		}
+	}
+	printf("\n\nitem:%d\n\n", item);
+	return 0;
+}
+
+
+
+
